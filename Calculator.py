fdb7b0755a302a865b3c9dc671bebde229fa0b97
ruchisharmaku-debug <ruchisharmaku@gmail.com>
2026-01-20 11:27:53 +0530

    Create a basic calculator in Python
    This script performs basic arithmetic operations including addition, subtraction, multiplication, division, and modulus.

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+# CREATE A CALCULATOR
+
+a = int(input("Enter First Number:"))
+b = int(input("Enter your second number:"))
+print("Addition =", a+b)
+print("Substraction=", a-b)
+print("Multiplication=",a*b)
+print("Division",a/b)
+print("Modulous",a%b)
+
+
+OUTPUT:
+Enter First Number:5
+Enter your second number:10
+Addition = 15
+Substraction= -5
+Multiplication= 50
+Division 0.5
+Modulous 5
